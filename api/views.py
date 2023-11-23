7b612c7e53c2764dafbbd68f4a6ad520770d8854
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-11-23 23:22:41 +0330

    added a filtering ability in which users can search among houses available

--- api/views.py
+++ api/views.py
@@ -2,10 +2,20 @@ from rest_framework.viewsets import ModelViewSet
 from .serializer import HouseSerializer
 from .models import HouseModel
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 
-# Create your views here.
+class HousePagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
 class HouseViewSet(ModelViewSet):
-    permission_classes = (IsAuthenticated,)
     queryset = HouseModel.objects.all()
+    permission_classes = (IsAuthenticated,)
+    pagination_class = HousePagination
     serializer_class = HouseSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("area", "floor", "city", "price", "status")
