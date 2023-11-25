1e29e16ae1c84cd47428a37ecfa4ab9cb6f84fb1
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-11-25 12:53:40 +0330

    changed the main model fields a little

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 import uuid
 
 
@@ -11,12 +12,27 @@ class HouseModel(models.Model):
         ),
     )
     unique_id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
     )
     area = models.IntegerField()
     floor = models.IntegerField()
-    city = models.CharField(max_length=150, db_index=True)
+    city = models.CharField(max_length=50, db_index=True)
     price = models.FloatField()
     status = models.CharField(
-        max_length=50, choices=STATUS_CHOICE, db_index=True
+        max_length=50, choices=STATUS_CHOICE, default="FREE", db_index=True
     )
+    seller = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.DO_NOTHING,
+        null=False,
+        related_name="seller",
+    )
+    buyer = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="buyer",
+    )
+
+    def __str__(self):
+        return f"House {self.unique_id} in {self.city}"
