ffad887da6e1e0f876f860ba0add9273a0b69c65
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-11-25 12:55:30 +0330

    changed representaion of user information

--- users/serializers.py
+++ users/serializers.py
@@ -17,6 +17,16 @@ class UserSerializer(ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
+    def to_representation(self, instance):
+        if (
+            self.context.get("request")
+            and self.context["request"].method == "GET"
+        ):
+            data = super().to_representation(instance)
+            data.pop("password", None)
+            return data
+        return super().to_representation(instance)
+
 
 class MessageSerializer(ModelSerializer):
     class Meta:
