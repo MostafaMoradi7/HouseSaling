cc504cff627c736944880dc4adf870ca466f2a87
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-11-25 14:40:33 +0330

    fixed users password visibility

--- users/serializers.py
+++ users/serializers.py
@@ -13,19 +13,14 @@ class UserSerializer(ModelSerializer):
             "password",
         )
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation.pop("password", None)
+        return representation
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-
-    def to_representation(self, instance):
-        if (
-            self.context.get("request")
-            and self.context["request"].method == "GET"
-        ):
-            data = super().to_representation(instance)
-            data.pop("password", None)
-            return data
-        return super().to_representation(instance)
 
 
 class MessageSerializer(ModelSerializer):
